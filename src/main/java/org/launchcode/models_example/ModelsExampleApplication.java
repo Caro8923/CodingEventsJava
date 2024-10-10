2f78ad714c51f278652d4ca23475aab63533921c
Carolyn Smith <carolynsmith@slu.edu>
2024-10-09 22:10:00 -0500

    initial commit

---
+++ src/main/java/org/launchcode/models_example/ModelsExampleApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.models_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelsExampleApplication.class, args);
+	}
+
+}
